7097e08a4751ed12b59be780bb5512488b09a62d
Nobuki Sugito <nobupiiman@gmail.com>
2019-11-17 04:15:57 +0900

    pr2eus and libfreenect OK version

--- rosdocker-run.sh
+++ rosdocker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost local:
-docker run -ti --privileged --shm-size=8gb --env="QT_X11_NO_MITSHM=1" -v $(pwd)/catkin_ws:/catkin_ws -v $(pwd)/models:/root/.gazebo/models -v /run/udev:/run/udev -v /run/dbus:/run/dbus -v /run/systemd:/run/systemd -v "/tmp/.X11-unix/:/tmp/.X11-unix:rw" my-ros:4
+docker run -ti --privileged --shm-size=8gb --net=host --env="QT_X11_NO_MITSHM=1" -v $(pwd)/../../github/robot-system/beginner_tutorials:/catkin_ws/src/beginner_tutorials -v $(pwd)/models:/root/.gazebo/models -v /run/udev:/run/udev -v /run/dbus:/run/dbus -v /run/systemd:/run/systemd -v "/tmp/.X11-unix/:/tmp/.X11-unix:rw" my-ros:5
